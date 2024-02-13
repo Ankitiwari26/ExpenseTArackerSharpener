959fc7f62408973ba45cba7dd60ebf21a0675935
ankit <ankitiwari26@gmail.com>
2024-02-14 01:51:33 +0530

    Concept of Composition

--- src/Components/ExpenseDate.js
+++ src/Components/ExpenseDate.js
@@ -1,17 +1,18 @@
-import React from "react";
+import "./ExpenseDate.css";
+import Card from "./Card";
+
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
 
   return (
-    <div>
-      <div className="expense-date">
-        <div className="expense-date__month">{month}</div>
-        <div className="expense-date__day">{day}</div>
-        <div className="expense-date__year">{year}</div>
-      </div>
-    </div>
+    <Card className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </Card>
   );
 }
+
 export default ExpenseDate;
--- src/Components/Expenseitem.js
+++ src/Components/Expenseitem.js
@@ -1,17 +1,17 @@
-import "./ExpenseItem.css";
-import React from "react";
 import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div classNmae="expense-item_description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item_price">${props.amount}</div>
-        <div className="expense-item_loction">${props.location}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
+
 export default ExpenseItem;
