67c093b5084815fa76ba8add5f915b265fce7602
ankit <ankitiwari26@gmail.com>
2024-02-15 02:07:50 +0530

    Lifting the data 1

--- src/App.js
+++ src/App.js
@@ -23,11 +23,15 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expense) => {
+    console.log("In app.js");
+    console.log(expense);
+  };
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
