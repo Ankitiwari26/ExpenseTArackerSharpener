5415ff8e2ae1a697496772bd86343edc99ff83c5
ankit <ankitiwari26@gmail.com>
2024-02-14 10:43:51 +0530

    Created a eventhandler in  ExpenseItem.js file and printed clicked on console

--- src/Components/Expenseitem.js
+++ src/Components/Expenseitem.js
@@ -3,6 +3,10 @@ import "./ExpenseItem.css";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked me!!!!!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Click me</button>
     </Card>
   );
 }
